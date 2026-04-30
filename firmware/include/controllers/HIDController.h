dd4a53974aa141fd5053adc02b7960da2b6a201e
edilsoncorrea <edilsoncorrea117@gmail.com>
2026-04-30 20:57:16 -0300

    feat: native 3Dconnexion driver compatibility via HID protocol emulation

--- firmware/include/controllers/HIDController.h
+++ firmware/include/controllers/HIDController.h
@@ -10,18 +10,29 @@ class HIDController {
   bool sendReports(const float motion[6], uint16_t buttonBits);
 
  private:
-  struct __attribute__((packed)) ReportAxes {
-    int16_t x, y, z, rx, ry, rz;
+  // Report ID 1: Translation (X, Y, Z)
+  struct __attribute__((packed)) ReportTranslation {
+    int16_t x, y, z;
   };
 
+  // Report ID 2: Rotation (Rx, Ry, Rz)
+  struct __attribute__((packed)) ReportRotation {
+    int16_t rx, ry, rz;
+  };
+
+  // Report ID 3: Buttons (32 bits for up to 32 buttons)
   struct __attribute__((packed)) ReportButtons {
-    uint16_t bits;
+    uint8_t bits[4];
   };
 
-  static ReportAxes makeAxesReport(const float motion[6]);
-  bool axesReportChanged(const ReportAxes& axes) const;
+  static void makeReports(const float motion[6],
+                           ReportTranslation& trans,
+                           ReportRotation& rot);
+  bool reportsChanged(const ReportTranslation& trans,
+                      const ReportRotation& rot) const;
 
   Adafruit_USBD_HID usbHid_;
   uint16_t buttonBitsSent_ = 0;
-  ReportAxes lastSentAxes_{};
+  ReportTranslation lastSentTrans_{};
+  ReportRotation lastSentRot_{};
 };
